61643475df6b909cbe2c336e3cd8504658d6889b
NikulNayi-17 <nikulnayee1717@gmail.com>
2022-04-16 19:08:12 +0530

    second trial

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = [
-    'ecommerce-webapp-env.eba-2c5g7zf4.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
